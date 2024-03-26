51b7f15b168f326118f55764816446b1f2ec8b3a
João Pedro Correia <jpedrocorreia172@outlook.com>
2024-03-26 20:23:30 -0300

    first commit

---
+++ CalculadoraTeste.java
@@ -0,0 +1,12 @@
+public class CalculadoraTeste {
+    
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        // Testar soma
+        double soma = calc.somar(2, 3);
+        System.out.println(soma);
+        // Testar subtração
+        double sub = calc.subtrair(2, 3);
+        System.out.println(sub);
+    }
+}
